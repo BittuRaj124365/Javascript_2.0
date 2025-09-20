2a882187da3c19ab9379691fbf5a25b8a79fe0af
Bittu Raj <bitturaj124365@gmail.com>
2025-09-21 00:49:43 +0530

    only saw tutorials

---
+++ Basics/variables.js
@@ -0,0 +1,27 @@
+let accountNo=123
+const name="Bittu Raj"
+var regiNo=321
+accountNo=345
+regiNo=567
+// name="B2 Raj"
+// console.table([accountNo,name,regiNo])
+console.table([accountNo,name,regiNo])
+console.log(typeof(regiNo))
+console.log(typeof(null));
+console.log(undefined);
+
+const number="12345s"
+console.log(number)
+const stringInNumber=Number(number)
+console.log(stringInNumber)
+console.log(typeof(stringInNumber))
+
+let n="krishna"
+console.log(n.charAt(4))
+console.log(n.indexOf('i'))
+
+const date=new Date()
+console.log(date)
+console.log(date.toString());
+console.log(date.toLocaleDateString());
+console.log(date.toLocaleTimeString());
